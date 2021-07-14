ad98447b0565222f216dd12d445ae319be31df84
priyanka22-11-2001 <priyankaranasingh22@gmail.com>
2021-07-14 22:16:46 +0530

    task9

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+//fs.mkdirSync('priyanka_rana');
+//fs.writeFileSync("priyanka_rana/bio.txt","I am Priyanka rana");
+//fs.appendFileSync("priyanka_rana/bio.txt","I am in CSE brach second year!!!");
+//const data = fs.readFileSync("priyanka_rana/bio.txt","utf8");
+//console.log(data);
+//fs.renameSync("priyanka_rana/bio.txt","priyanka_rana/newbio.txt");
+//fs.unlinkSync("priyanka_rana/newbio.txt");
+fs.rmdirSync("priyanka_rana");
